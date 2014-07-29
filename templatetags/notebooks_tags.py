b6d38017828534c1e5533f2261b80dcac31ea6f7
Guillaume Luchet <guillaume@geelweb.org>
2014-07-29 21:06:15 +0200

    create add_to_notebook template tag

--- templatetags/notebooks_tags.py
+++ templatetags/notebooks_tags.py
@@ -14,6 +14,21 @@ class NotebookNode(template.Node):
             {'app_label': self.app_label, 'model': self.model},
             autoescape=context.autoescape))
 
+
+class AddToNotebookNode(template.Node):
+    def __init__(self, app_label, model, object_id):
+        self.app_label = app_label
+        self.model = model
+        self.object_id = template.Variable(object_id)
+
+    def render(self, context):
+        t = template.loader.get_template('notebooks/add_to_notebook_btn.html')
+        return t.render(template.Context({
+            'app_label': self.app_label,
+            'model': self.model,
+            'object_id': self.object_id.resolve(context),
+            }, autoescape=context.autoescape))
+
 @register.tag
 def notebook(parser, token):
     """
@@ -46,3 +61,36 @@ def notebook(parser, token):
 
     return NotebookNode(app_label=app_label[1:-1], model=model[1:-1])
 
+@register.tag
+def add_to_notebook(parser, token):
+    """
+    Display the button to add an item to the notebook
+
+    Syntax::
+
+        {% notebook "app_label" "model" object_id %}
+
+    Example::
+
+        {% notebook "catalog" "product" product.pk %}
+    """
+
+    bits = token.split_contents()
+    syntax_message = ("%(tag_name)s expects a syntax of %(tag_name)s "
+                      "'app_label' 'model' object_id" %
+                      dict(tag_name=bits[0]))
+
+    if len(bits) != 4:
+        raise template.TemplateSyntaxError(syntax_message)
+
+    app_label = bits[1]
+    if not (app_label[0] == app_label[-1] and app_label[0] in ('"', "'")):
+        raise template.TemplateSyntaxError("%r tag's argument should be in quotes" % bits[0])
+
+    model = bits[2]
+    if not (model[0] == model[-1] and model[0] in ('"', "'")):
+        raise template.TemplateSyntaxError("%r tag's argument should be in quotes" % bits[0])
+
+    object_id = bits[3]
+
+    return AddToNotebookNode(app_label=app_label[1:-1], model=model[1:-1], object_id=object_id)

c043286848abed1347522168e8316d0b3ceec0c5
Guillaume Luchet <guillaume@geelweb.org>
2014-07-29 22:58:48 +0200

    Add notebooks.NotebookItem model to store prefered stuff in db if the user is logged in

--- models.py
+++ models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.utils.translation import ugettext as _
 
-# Create your models here.
+
+class NotebookItem(models.Model):
+    user = models.ForeignKey(User)
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+
+    date_created = models.DateTimeField(auto_now=False, auto_now_add=True)
--- views.py
+++ views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.contrib.contenttypes.models import ContentType
 from django import template
+from django.core import serializers
+from notebooks.models import NotebookItem
 
 
 def item(request, app_label, model):
@@ -16,3 +18,18 @@ def item(request, app_label, model):
 
 
     return HttpResponse(t.render(template.RequestContext(request, {'item': item})))
+
+def store(request, app_label, model, pk):
+    if request.user.is_authenticated():
+        content_type = ContentType.objects.get(app_label=app_label, model=model)
+        NotebookItem.objects.get_or_create(user=request.user, content_type=content_type, object_id=pk)
+    return HttpResponse()
+
+def load(request, app_label, model):
+    if request.user.is_authenticated():
+        content_type = ContentType.objects.get(app_label=app_label, model=model)
+        collection = NotebookItem.objects.filter(user=request.user, content_type=content_type)
+        return HttpResponse(
+                serializers.serialize("json", collection),
+                content_type='application/json')
+    return HttpResponse('[]', content_type='application/json')

63a06468e3a65c77ba42641f12d0e09a5eb8b176
Guillaume Luchet <guillaume@geelweb.org>
2014-07-29 12:03:11 +0200

    make notebooks app more generic

--- views.py
+++ views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from ads.models import Ad
+from django.http import HttpResponse
+from django.contrib.contenttypes.models import ContentType
+from django import template
 
 def notebook(request):
     return render_to_response('notebooks/notebook.html',
                               context_instance=RequestContext(request))
 
-def item(request):
+def item(request, app_label, model):
     pk = request.GET['pk']
-    ad = Ad.objects.get(pk=pk)
 
-    return render_to_response('notebooks/item.html',
-                              {'ad': ad},
-                              context_instance=RequestContext(request))
+    content_type = ContentType.objects.get(app_label=app_label, model=model)
+    item = content_type.get_object_for_this_type(pk=pk)
+
+    t = template.loader.select_template([
+        '%s/notebooks/item.html' % app_label,
+        'notebooks/item.html',
+    ])
+
+
+    return HttpResponse(t.render(template.RequestContext(request, {'item': item})))
